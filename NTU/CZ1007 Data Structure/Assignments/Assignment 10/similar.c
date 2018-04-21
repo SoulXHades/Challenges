b2e517b487adefee36c4a06519cecd16aad9567b
SoulX <haojie_34@hotmail.com>
2018-04-21 11:33:53 +0800

    Data Structures
    Tidy up checklist and similar.c

--- NTU/CZ1007 Data Structure/Assignments/Assignment 10/similar.c
+++ NTU/CZ1007 Data Structure/Assignments/Assignment 10/similar.c
@@ -170,21 +170,12 @@ int main()
 int identical(BTNode *tree1, BTNode *tree2)
 
 {
-   if(tree1 == NULL)
-   {
-       if(tree2 == NULL)
-        return 1;
-       else
-        return 0;
-   }
-   else
-   {
-       if(tree2 == NULL)
-        return 0;
-   }
-
-    if(tree1->item != tree2->item)
-        return 0;
+   if(tree1 == NULL && tree2 == NULL)
+    return 1;
+   else if(tree1 == NULL || tree2 == NULL)
+    return 0;
+   else if(tree1->item != tree2->item)
+    return 0;
 
     return identical(tree1->left, tree2->left) && identical(tree1->right, tree2->right);
 }
